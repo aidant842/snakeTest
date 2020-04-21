398aa830494b73a7369e3ad9f6899d0fc4ca062a
aidant842 <aidant842@gmail.com>
2020-04-21 13:29:36 +0000

    temporary fix for canvas not rendering at correct size

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -2,6 +2,8 @@
 
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
+/* ctx.canvas.width  = 300;
+ctx.canvas.height  = 300; */
 const scale = 20;
 const rows = canvas.height / scale;
 const colums = canvas.width / scale;
